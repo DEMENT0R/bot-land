bf19af0829ce1bf73c629b81c94c5f152b412ff2
DEMENTOR <dmntr@rambler.ru>
2019-01-26 00:10:48 +1000

    tabs

--- partials/sections/section-2.php
+++ partials/sections/section-2.php
@@ -29,24 +29,28 @@
 				      <h3 class="tomoru-font-2">Возможно автоматизировать  до 100% продаж</h3>
 				      <div class="row">
 				      	<div class="col-12 col-md-3 row wow fadeInUp">
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
+				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
+				      		<div class="col-4"><img src="img/icons/icon-1-1.svg" alt=""></div>
 				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
 				      	</div>
 				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
+				      		<div class="col-4"><img src="img/icons/icon-1-2.svg" alt=""></div>
+				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Обрабатывает заявки с сайта</p></div>
+				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот перезванивает по заявке с сайта</div></div>
 				      	</div>
 				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
+				      		<div class="col-4"><img src="img/icons/icon-1-3.svg" alt=""></div>
+				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Коммерческое предложение</p></div>
+				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот комментирует предложение и отвечает на вопросы</div></div>
 				      	</div>
 				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
+				      		<div class="col-4"><img src="img/icons/icon-1-4.svg" alt=""></div>
+				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3" style="font-weight: 600;">Согласие на покупку</p></div>
+				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот формирует документы на сделку и оплату</div></div>
 				      	</div>
 				      </div>
 				    </div>
@@ -55,21 +59,25 @@
 				      <p class="tomoru-font-3">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				      <div class="row">
 				      	<div class="col-12 col-md-3 row wow fadeInUp">
+				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
 				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
 				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
 				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
 				      	</div>
 				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
+				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
 				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
 				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
 				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
 				      	</div>
 				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
+				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
 				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
 				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
 				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
 				      	</div>
 				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
+				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
 				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
 				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
 				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
