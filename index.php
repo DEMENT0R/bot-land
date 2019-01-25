ee969ba25edad38feb04a53e1a29e5d50a2e87b7
DEMENTOR <dmntr@rambler.ru>
2019-01-25 21:46:44 +1000

    slashes fix...

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 
-include 'partials\start.php';
+include 'partials/start.php';
 //include 'partials\header.php';
-include 'partials\body.php';
+include 'partials/body.php';
 //include 'partials\footer.php';
-include 'partials\end.php';
+include 'partials/end.php';
 
 ?>
