418330f8ca01495d2d20308b63b29ebe294f5184
DEMENTOR <dmntr@rambler.ru>
2019-01-27 17:36:40 +1000

    Update section-5.php

--- partials/sections/section-5.php
+++ partials/sections/section-5.php
@@ -1,3 +1,48 @@
           <div class="bg_all bg_5">
-              <div class="decoy">5</div>
+          	<!-- <div class="decoy">5</div> -->
+          	<div class="col-12 col-md-10 offset-md-2 row">
+          		<div class="col-12 row">
+          			<div class="col-12">
+          				<h2 class="tomoru-font-1 wow fadeInDown">Цены</h2>
+          			</div>
+          			<div class="col-12">
+          				<h3 class="tomoru-font-2">Рассчитайте примерную стоимость вашего проекта</h3>
+          			</div>
+          			<div class="col-12 row">
+          				<div class="col-12 col-md-8">
+          					<div class="col-12 row">
+          						<p class="tomoru-font-3 mb-0">Тип бота <img src="img/icons/question.svg" alt="Помощь"></p>
+								<div class="btn-group row col-12" role="group" aria-label="Basic example">
+								  <button type="button" class="btn btn-success tomoru-btn-2 col-4 tomoru-font-4">Текстовый</button>
+								  <button type="button" class="btn btn-success tomoru-btn-2 col-4 tomoru-font-4">Голосовой стандартный</button>
+								  <button type="button" class="btn btn-success tomoru-btn-2 col-4 tomoru-font-4">Голосовой эксклюзивный</button>
+								</div>
+          					</div>
+        					<div class="col-12 row">
+          						<p class="tomoru-font-3 mb-0">Уровень обучаемости <img src="img/icons/question.svg" alt="Помощь"></p>
+								<div class="btn-group row col-12" role="group" aria-label="Basic example">
+								  <button type="button" class="btn btn-success tomoru-btn-2 col-4 tomoru-font-4">Базовый</button>
+								  <button type="button" class="btn btn-success tomoru-btn-2 col-4 tomoru-font-4">Продвинутый</button>
+								  <button type="button" class="btn btn-success tomoru-btn-2 col-4 tomoru-font-4">Эксклюзивный</button>
+								</div>
+          					</div>
+          					<div class="col-12 row">
+          						<p class="tomoru-font-3">
+          							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum deleniti, aspernatur obcaecati repellendus sequi, eos eaque expedita odio saepe quos, deserunt similique! Beatae incidunt fuga dolorum placeat consectetur vel sed.
+          						</p>
+          					</div>
+          				</div>
+          				<div class="col-12 col-md-4">
+          					<p class="tomoru-font-3">
+          						<span style="font-weight: 600;">100 000 ₽</span><br>
+          						Стоимость внедрения (без учета вашей скидки)<br>
+          						<span style="font-weight: 600;">6 ₽</span><br>
+          						Стоимость минуты разговора<br>
+          						<span style="font-weight: 600;">40%</span><br>
+          						Возможная экономия на сотрудниках
+          					</p>
+          				</div>
+          			</div>
+          		</div>
+          	</div>
           </div>
