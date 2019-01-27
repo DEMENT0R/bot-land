432d2fda86d91959e09a7e39aa921db226a0a23e
DEMENTOR <dmntr@rambler.ru>
2019-01-27 15:16:22 +1000

    Update section-3.php

--- partials/sections/section-3.php
+++ partials/sections/section-3.php
@@ -1,35 +1,23 @@
           <div class="bg_all bg_3 row">
           	<!-- <div class="decoy">3</div> -->
-          	<div class="col-12 col-md-10 offset-md-2 row">
-          		<div class="col-12">
-          			<h2 class="tomoru-font-1 wow fadeInDown">Настройте бота в три шага</h2>
-          			<h3 class="tomoru-font-2 ">Бот автоматически обучается на основе ваших данных и своего опыта</h3>
-          		</div>
-          		<div class="col-12 row">
-          			<div class="col-12 col-md-3 row wow fadeInUp">
-          				<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
-          				<div class="col-4"><img src="img/icons/icon-1-1.svg" alt=""></div>
-          				<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-          				<div class="col-12 p-0"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
-          			</div>
-          			<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
-          				<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
-          				<div class="col-4"><img src="img/icons/icon-1-2.svg" alt=""></div>
-          				<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Обрабатывает заявки с сайта</p></div>
-          				<div class="col-12 p-0"><div class="tomoru-font-3">Бот перезванивает по заявке с сайта</div></div>
-          			</div>
-          			<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
-          				<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
-          				<div class="col-4"><img src="img/icons/icon-1-3.svg" alt=""></div>
-          				<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Коммерческое предложение</p></div>
-          				<div class="col-12 p-0"><div class="tomoru-font-3">Бот комментирует предложение и отвечает на вопросы</div></div>
-          			</div>
-          			<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
-          				<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
-          				<div class="col-4"><img src="img/icons/icon-1-4.svg" alt=""></div>
-          				<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3" style="font-weight: 600;">Согласие на покупку</p></div>
-          				<div class="col-12 p-0"><div class="tomoru-font-3">Бот формирует документы на сделку и оплату</div></div>
-          			</div>
-          		</div>
-          	</div>
+              <div class="col-12 col-md-10 offset-md-2 row text-left">
+                <h2 class="tomoru-font-1 wow fadeInDown">Поддерживают диалог с клиентами</h2>
+                <p class="tomoru-font-2 wow fadeInLeft">Диалоговый интерфейс с информационными системами в виде текста и голоса</p>
+
+                <!-- <img src="img/macbook.png" alt="" align="right" width="40%" class="d-md-none"> -->
+                 <div class="col-12 d-md-none">
+                    <img src="img/phone.png" width="100%" class="d-md-none">
+                  </div>
+                <div class="col-12 row pr-0 mr-0 wow fadeInRight">
+                    <div class="col-3"><img src="img/icons/check.svg"></div>
+                    <div class="col-9 pr-0">
+                      <p class="tomoru-font-3">
+                        <b style="font-weight: 600;">Никогда не забывает</b><br class="d-none d-md-block">
+                        Перезвонить, отправить предложение<br class="d-none d-md-block"> или выставить счёт
+                      </p>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4"><a href="#" class="tomoru-font-3 tomoru-btn-1 wow fadeInUp">Создать бота</a></div>
+              </div>
           </div>
