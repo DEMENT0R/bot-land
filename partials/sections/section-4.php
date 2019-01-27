f048bfcdf0e3278f847f7952c17bc11a7cab1f44
DEMENTOR <dmntr@rambler.ru>
2019-01-27 15:59:35 +1000

    Update section-4.php

--- partials/sections/section-4.php
+++ partials/sections/section-4.php
@@ -2,8 +2,78 @@
               <!-- <div class="decoy">4</div> -->
           	<div class="col-12 col-md-10 offset-md-2 row">
           		<div class="col-12 row">
-          			<h2 class="tomoru-font-1 wow fadeInDown">Примеры внедрений</h2><br>
-          			<h3 class="tomoru-font-2 ">Умный бот «LIKE Центр»</h3>
+          			<div class="col-12">
+          				<h2 class="tomoru-font-1 wow fadeInDown">Примеры внедрений</h2>
+          			</div>
+          			<div class="col-12">
+          				<h3 class="tomoru-font-2 "><img src="img/like-center.png" alt="Лайк-центр" class="mr-3">Умный бот «LIKE Центр»</h3>
+          			</div>
+          			<div class="col-12 row">
+          				<div class="col-12 col-md-6">
+          					<div class="col-12 row">
+          						<div class="col-3"><img src="img/alex-hvostenko.png" alt="Александр Хвостенко" align="left" width="80%"></div>
+          						<div class="col-9 my-auto">
+	          						<p class="tomoru-font-3">
+	          							<span style="font-weight: 600;">Александр Хвостенко</span><br>
+	          							супер-маркетоголог  «LIKE Центр»
+	          						</p>
+          						</div>
+          					</div>
+          					<div class="col-12 row">
+	         					<p class="tomoru-font-3">
+	          						Спасибо команде Gexar за отличного умного чат-бота для нашей самой масштабной конференции — "Концентрат”. <br>
+									Чат-бот отлично справился с нагрузкой, мы чувствовали поддержку команды буквально 24/7. Продукт, сопровождение и отношение к делу — сильнейшие стороны команды, а лучшей оценкой с нашей стороны – это рекомендации, которые мы даем, и последующие проекты реализуемые с командой Gexar».
+	          					</p>
+          					</div>
+          				</div>
+          				<div class="col-12 col-md-6">
+          					<p class="tomoru-font-3">
+          						<span style="font-weight: 600;">Цель</span><br>
+          						Консультация посетителей конференции «Концентрат»<br>
+          						<span style="font-weight: 600;">Уровень обучения</span><br>
+								Обучен понимать и отвечать на 122 темы<br>
+								<span style="font-weight: 600;">Срок реализации: 4 недели</span>
+          					</p>
+          				</div>
+          			</div>
+          			<div class="col-12 row">
+          				<div class="col-12 col-md-3 row">
+          					<div class="col-4"><img src="img/icons/icon-1-1.svg" alt="" width="100%"></div>
+          					<div class="col-8">
+          						<p class="tomoru-font-2" style="color: #06A77D;">15 000 +</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-4">Участников конференции «Концентрат»</p>
+          					</div>
+          				</div>
+          				<div class="col-12 col-md-3 row">
+          					<div class="col-4"><img src="img/icons/icon-1-1.svg" alt="" width="100%"></div>
+          					<div class="col-8">
+          						<p class="tomoru-font-2" style="color: #06A77D;">2 688</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-4">Участников конференции «Концентрат»</p>
+          					</div>
+          				</div>
+          				<div class="col-12 col-md-3 row">
+          					<div class="col-4"><img src="img/icons/icon-1-1.svg" alt="" width="100%"></div>
+          					<div class="col-8">
+          						<p class="tomoru-font-2" style="color: #06A77D;">82%</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-4">Участников конференции «Концентрат»</p>
+          					</div>
+          				</div>
+          				<div class="col-12 col-md-3 row">
+          					<div class="col-4"><img src="img/icons/icon-1-1.svg" alt="" width="100%"></div>
+          					<div class="col-8">
+          						<p class="tomoru-font-2" style="color: #06A77D;">77%</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-4">Участников конференции «Концентрат»</p>
+          					</div>
+          				</div>
+          			</div>
           		</div>
           	</div>
           </div>
