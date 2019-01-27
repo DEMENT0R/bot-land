babfef962b63946cefa3e6a6b09f87b50f02e1b4
DEMENTOR <dmntr@rambler.ru>
2019-01-27 17:36:38 +1000

    Update section-2.php

--- partials/sections/section-2.php
+++ partials/sections/section-2.php
@@ -1,101 +1,104 @@
           <div class="bg_all bg_2 row">
-              <!-- <div class="decoy">2</div> -->
-            <div class="col-12 col-md-10 offset-md-2 row">
-            	<div class="col-12">
-				  <h2 class="tomoru-font-1 col-12 wow fadeInDown">Поставьте бизнес на автопилот</h2>
-				  <br>
-				  <!-- Nav tabs -->
-				  <ul class="nav nav-tabs wow fadeInLeft">
-				    <li class="nav-item">
-				      <a class="nav-link active" data-toggle="tab" href="#menu1">Отдел продаж</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" data-toggle="tab" href="#menu2">Отдел кадров</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" data-toggle="tab" href="#menu3">Техническая поддержка</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" data-toggle="tab" href="#menu4">Справочная служба</a>
-				    </li>
-				    <li class="nav-item">
-				      <a class="nav-link" data-toggle="tab" href="#menu5">Другие задачи</a>
-				    </li>
-				  </ul>
+          	<!-- <div class="decoy">2</div> -->
+          	<div class="col-12 col-md-10 offset-md-2 row">
+          		<div class="col-12">
+          			<h2 class="tomoru-font-1 col-12 wow fadeInDown">Поставьте бизнес на автопилот</h2>
+          			<br>
+          			<!-- Nav tabs -->
+          			<ul class="nav nav-tabs wow fadeInLeft">
+          				<li class="nav-item">
+          					<a class="nav-link active" data-toggle="tab" href="#menu1">Отдел продаж</a>
+          				</li>
+          				<li class="nav-item">
+          					<a class="nav-link" data-toggle="tab" href="#menu2">Отдел кадров</a>
+          				</li>
+          				<li class="nav-item">
+          					<a class="nav-link" data-toggle="tab" href="#menu3">Техническая поддержка</a>
+          				</li>
+          				<li class="nav-item">
+          					<a class="nav-link" data-toggle="tab" href="#menu4">Справочная служба</a>
+          				</li>
+          				<li class="nav-item">
+          					<a class="nav-link" data-toggle="tab" href="#menu5">Другие задачи</a>
+          				</li>
+          			</ul>
 
-				  <!-- Tab panes -->
-				  <div class="col-12 row tab-content wow fadeInRight">
-				    <div id="menu1" class="col-12 tab-pane active"><br>
-				      <h3 class="tomoru-font-2">Возможно автоматизировать  до 100% продаж</h3>
-				      <div class="row">
-				      	<div class="col-12 col-md-3 row wow fadeInUp">
-				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
-				      		<div class="col-4"><img src="img/icons/icon-1-1.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
-				      	</div>
-				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
-				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
-				      		<div class="col-4"><img src="img/icons/icon-1-2.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Обрабатывает заявки с сайта</p></div>
-				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот перезванивает по заявке с сайта</div></div>
-				      	</div>
-				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
-				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
-				      		<div class="col-4"><img src="img/icons/icon-1-3.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Коммерческое предложение</p></div>
-				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот комментирует предложение и отвечает на вопросы</div></div>
-				      	</div>
-				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
-				      		<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
-				      		<div class="col-4"><img src="img/icons/icon-1-4.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3" style="font-weight: 600;">Согласие на покупку</p></div>
-				      		<div class="col-12 p-0"><div class="tomoru-font-3">Бот формирует документы на сделку и оплату</div></div>
-				      	</div>
-				      </div>
-				    </div>
-				    <div id="menu2" class="col-12 tab-pane fade"><br>
-				      <h3 class="tomoru-font-2">Возможно автоматизировать  до 100% на предварительном этапе подбора</h3>
-				      <p class="tomoru-font-3">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				      <div class="row">
-				      	<div class="col-12 col-md-3 row wow fadeInUp">
-				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
-				      	</div>
-				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
-				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
-				      	</div>
-				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
-				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
-				      	</div>
-				      	<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
-				      		<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
-				      		<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
-				      		<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
-				      		<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
-				      	</div>
-				      </div>
-				    </div>
-				    <div id="menu3" class="col-12 tab-pane fade"><br>
-				      <h3 class="tomoru-font-2">Возможно автоматически решать до 70% обращений в техподдержку</h3>
-				      <p class="tomoru-font-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-				    </div>
-				    <div id="menu4" class="col-12 tab-pane fade"><br>
-				      <h3 class="tomoru-font-2">Возможно автоматически решать до 70% обращений в справочную службу</h3>
-				      <p class="tomoru-font-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-				    </div>
-				    <div id="menu5" class="col-12 tab-pane fade"><br>
-				      <p class="tomoru-font-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-				    </div>
-				  </div>
-				</div>
-            </div>
+          			<!-- Tab panes -->
+          			<div class="col-12 row tab-content wow fadeInRight">
+          				<div id="menu1" class="col-12 tab-pane active"><br>
+          					<h3 class="tomoru-font-2">Возможно автоматизировать  до 100% продаж</h3>
+          					<div class="row">
+          						<div class="col-12 col-md-3 row wow fadeInUp">
+          							<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
+          							<div class="col-4"><img src="img/icons/icon-1-1.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
+          							<div class="col-12 p-0"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+          						</div>
+          						<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
+          							<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
+          							<div class="col-4"><img src="img/icons/icon-1-2.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Обрабатывает заявки с сайта</p></div>
+          							<div class="col-12 p-0"><div class="tomoru-font-3">Бот перезванивает по заявке с сайта</div></div>
+          						</div>
+          						<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
+          							<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt=""></div>
+          							<div class="col-4"><img src="img/icons/icon-1-3.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">Коммерческое предложение</p></div>
+          							<div class="col-12 p-0"><div class="tomoru-font-3">Бот комментирует предложение и отвечает на вопросы</div></div>
+          						</div>
+          						<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
+          							<div class="col-10 offset-2 d-none d-md-block"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
+          							<div class="col-4"><img src="img/icons/icon-1-4.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3" style="font-weight: 600;">Согласие на покупку</p></div>
+          							<div class="col-12 p-0"><div class="tomoru-font-3">Бот формирует документы на сделку и оплату</div></div>
+          						</div>
+          					</div>
+          				</div>
+          				<div id="menu2" class="col-12 tab-pane fade"><br>
+          					<h3 class="tomoru-font-2">Возможно автоматизировать  до 100% на предварительном этапе подбора</h3>
+          					<p class="tomoru-font-3">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          					<div class="row">
+          						<div class="col-12 col-md-3 row wow fadeInUp">
+          							<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
+          							<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
+          							<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+          						</div>
+          						<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="1s">
+          							<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
+          							<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
+          							<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+          						</div>
+          						<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="2s">
+          							<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt=""></div>
+          							<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
+          							<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+          						</div>
+          						<div class="col-12 col-md-3 row wow fadeInUp" data-wow-delay="3s">
+          							<div class="col-10 offset-2"><img src="img/icons/curve-arrow.svg" alt="" class="invisible"></div>
+          							<div class="col-4"><img src="img/icons/check.svg" alt=""></div>
+          							<div class="col-8 p-0 m-0 pt-2"><p class="tomoru-font-3">На сайте компании</p></div>
+          							<div class="col-12"><div class="tomoru-font-3">Бот в переписывается с посетителем в чате сайта</div></div>
+          						</div>
+          					</div>
+          				</div>
+          				<div id="menu3" class="col-12 tab-pane fade"><br>
+          					<h3 class="tomoru-font-2">Возможно автоматически решать до 70% обращений в техподдержку</h3>
+          					<p class="tomoru-font-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+          				</div>
+          				<div id="menu4" class="col-12 tab-pane fade"><br>
+          					<h3 class="tomoru-font-2">Возможно автоматически решать до 70% обращений в справочную службу</h3>
+          					<p class="tomoru-font-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+          				</div>
+          				<div id="menu5" class="col-12 tab-pane fade"><br>
+          					<p class="tomoru-font-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+          				</div>
+          			</div>
+          			<div class="col-12 row mt-5">
+          				<a href="#" class="tomoru-font-3 tomoru-btn-1 wow fadeInUp">Автоматизировать мой бизнес</a>
+          			</div>
+          		</div>
+          	</div>
           </div>
