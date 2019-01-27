88293f297115bd5cc36b544c2403821c124252e6
DEMENTOR <dmntr@rambler.ru>
2019-01-27 18:30:57 +1000

    Update section-3.php

--- partials/sections/section-3.php
+++ partials/sections/section-3.php
@@ -1,23 +1,15 @@
           <div class="bg_all bg_3 row">
           	<!-- <div class="decoy">3</div> -->
               <div class="col-12 col-md-10 offset-md-2 row text-left">
-                <h2 class="tomoru-font-1 wow fadeInDown">Поддерживают диалог с клиентами</h2>
-                <p class="tomoru-font-2 wow fadeInLeft">Диалоговый интерфейс с информационными системами в виде текста и голоса</p>
+                <h2 class="tomoru-font-1" style="color: #FFFFFF !important; height: 48px;">Настройте бота в три шага</h2>
+                <p class="tomoru-font-2" style="color: #FFFFFF !important; height: 35px;">Бот автоматически обучается на основе ваших данных и своего опыта</p>
 
-                <!-- <img src="img/macbook.png" alt="" align="right" width="40%" class="d-md-none"> -->
-                 <div class="col-12 d-md-none">
-                    <img src="img/phone.png" width="100%" class="d-md-none">
-                  </div>
-                <div class="col-12 row pr-0 mr-0 wow fadeInRight">
-                    <div class="col-3"><img src="img/icons/check.svg"></div>
-                    <div class="col-9 pr-0">
-                      <p class="tomoru-font-3">
-                        <b style="font-weight: 600;">Никогда не забывает</b><br class="d-none d-md-block">
-                        Перезвонить, отправить предложение<br class="d-none d-md-block"> или выставить счёт
-                      </p>
-                    </div>
+                <div class="col-12 row">
+                    <img src="img/block-3.png" alt="" width="100%" height="auto">
                 </div>
 
-                <div class="col-12 col-md-4"><a href="#" class="tomoru-font-3 tomoru-btn-1 wow fadeInUp">Создать бота</a></div>
+                <div class="col-12">
+                  <a href="#" class="tomoru-font-3 tomoru-btn-1" style="background-color: #FFFFFF !important;">Создать бота</a>
+                </div>
               </div>
           </div>
