997727cbf80b247c5dfa788871ae5f7c78abc187
DEMENTOR <dmntr@rambler.ru>
2019-01-27 18:06:02 +1000

    Update section-6.php

--- partials/sections/section-6.php
+++ partials/sections/section-6.php
@@ -1,3 +1,56 @@
           <div class="bg_all bg_6">
-              <div class="decoy">6</div>
+              <!-- <div class="decoy">6</div> -->
+          	<div class="col-12 col-md-10 offset-md-2 row">
+          		<div class="col-12 row">
+          			<div class="col-12">
+          				<h2 class="tomoru-font-1 wow fadeInDown">Мы даём гарантии</h2>
+          			</div>
+          			<div class="col-12">
+          				<h3 class="tomoru-font-2">Вы платите только за успешную работу бота</h3>
+          			</div>
+          			<div class="col-12 row mt-3">
+          				<div class="col-4 row">
+          					<div class="col-3">
+          						<img src="img/icons/check.svg" alt="">
+          					</div>
+          					<div class="col-9">
+          						<p class="tomoru-font-5">40% вопросов решает сам</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-3">Даем рекомендации по созданию сопутствующих материалов, информации, позволяющих решить запрос человека без участия сотрудников компании</p>
+          					</div>
+          				</div>
+          				<div class="col-4 row">
+          					<div class="col-3">
+          						<img src="img/icons/check.svg" alt="">
+          					</div>
+          					<div class="col-9">
+          						<p class="tomoru-font-5">80% сообщений распознает</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-3">Минимальный уровень — мы не останавливаемся на нем, но гарантируем, что он будет достигнут на выборке 1000 диалогов</p>
+          					</div>
+          				</div>
+          				<div class="col-4 row">
+          					<div class="col-3">
+          						<img src="img/icons/check.svg" alt="">
+          					</div>
+          					<div class="col-9">
+          						<p class="tomoru-font-5">Начинает работать за 99 дней</p>
+          					</div>
+          					<div class="col-12">
+          						<p class="tomoru-font-3">Если через 999 дней мы не запустили бота, мы начинает возвращать вам деньги</p>
+          					</div>
+          				</div>
+          			</div>
+          			<div class="col-12 row">
+          				<a href="#" class="tomoru-font-3 tomoru-btn-1 mt-3 wow fadeInUp">Создать бота</a>
+          			</div>
+          			<div class="col-12 row">
+          				<div class="tomoru-font-4 mt-4">* Все сообщения, на которые бот не сумел ответить корректно, вы не оплачиваете.</div>
+          				<div class="tomoru-font-4">** Гарантии предоставляются на ботов созданных при участии специалистов нашей компании или наших партнеров.
+</div>
+          			</div>
+          		</div>
+          	</div>
           </div>
